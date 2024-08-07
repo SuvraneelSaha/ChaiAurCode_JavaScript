0ff87672535b8051debe646b351a36b7e9aad14d
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-08-07 05:15:43 +0000

    Ecma Dataypes 7th Aug

---
+++ O2_DatatypesAndECMAStandards/O1_Ecma.js
@@ -0,0 +1,38 @@
+"use strict"; // using this we are using the new version of  js 
+
+//alert(3+3); // not available in Node js but it runs in browser engine as 
+// as the js engine v8 is inbuilt in Browser 
+
+// Important - Readability is very Importnat 
+
+console.log(3+3)
+console.log(3
+  +3
+);
+
+// this above one is not a bad practice , and the code readability is a big thing 
+
+console.log("Nigga");
+
+// null --> it is a standalone value , Basically it means empty 
+// undefined => it means that a var is not defined , 
+// symbol --> unique datatype 
+
+// Non primitive datatype 
+// Object - 
+
+let age = 18 ;
+let naam = "Aggin";
+
+console.log(typeof naam); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
