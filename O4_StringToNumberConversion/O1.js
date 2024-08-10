4bf428ff8cafd6523be73ce264136335c946a145
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-08-10 06:36:30 +0000

    String to Number Conversion

---
+++ O4_StringToNumberConversion/O1.js
@@ -0,0 +1,71 @@
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+ //console.log(negValue);
+
+ // This above is basic stuffs 
+
+
+// console.log(2+2);
+// Basic Addition 
+// console.log(2-2);
+// Basic subtraction
+// console.log(2*2);
+// Basic Multiplication
+//  console.log(2**3);
+// 2 ^ 3 -- power operator in js is --> ** 
+// console.log(2/3);
+// As per normal standard it will be 0 , but it gives the exact value as 0.6666 
+// console.log(2%3);
+// remainder will be 2 
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+// Basic string concatenation 
+
+// console.log("1" + 2);
+// If String is in first it will convert the whole line 
+
+// console.log(1 + "2");
+// Same principle - if String is in Front it will convert the whole line 
+// but in this case only two operands are present so for which it is turning it into a String as a Whole 
+
+// console.log("1" + 2 + 2);
+// as the String operand is in first it will convert the whole into a String 
+
+// console.log(1 + 2 + "2");
+// ans will be 32 
+// as 1 and 2 operands are in first then at the last is the String 
+
+// console.log( (3 + 4) * 5 % 3);
+// these types of equations are not allowed nor should be encouraged in Production 
+// Writing clean readable code is the most important 
+
+
+// console.log(+true);
+// the ans will be 1  
+
+console.log(+""); 
+
+// + is the conversion agent 
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// these should not Be Encouraged 
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+// Link for Postfix and Prefix - Search postfix and prefix js mdn or Yt is there 
