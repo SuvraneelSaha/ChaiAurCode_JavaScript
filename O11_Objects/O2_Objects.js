35fa4937193d1fc3f2f13aa94a57a269b807bcce
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-08-16 21:58:46 +0530

    Create O2_Objects.js

---
+++ O11_Objects/O2_Objects.js
@@ -0,0 +1,60 @@
+// const tinderUser = new Object()
+// this one is singleton object
+// below one is not a singleton object
+const tinderUser = {};
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "some@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "hitesh",
+      lastname: "choudhary",
+    },
+  },
+};
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj4 = { 5: "a", 6: "b" };
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// {} - empty object is used as a Target
+// rest of the objects act as the source
+// Another way is spread operator
+const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+const users = [
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+];
+
+users[1].email;
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// returns the keys as an Array
+// console.log(Object.values(tinderUser));
+// return the values as an Array
+console.log(Object.entries(tinderUser));
+// returns the key and value as a pair in the form of an Array in a Whole array
+// [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
