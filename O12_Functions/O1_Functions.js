9a58c7fe326777e840716d4a20d6899f201545dd
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-08-17 06:42:49 +0530

    Functions - paramters , return statement , arguments

---
+++ O12_Functions/O1_Functions.js
@@ -0,0 +1,41 @@
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+// for execution we need to call the function 
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "sam"){
+  // sam is the default value 
+    if(!username){
+      // or use as username === undefined in the if statement 
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage())
+// in this case undefined gets returned 
