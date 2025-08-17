7fe31b79d50e8e3a8234c3892a3ca0283d92bb9e
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-08-17 06:53:20 +0530

    Functions with Arrays and Objects

--- O12_Functions/O1_Functions.js
+++ O12_Functions/O1_Functions.js
@@ -1,41 +1,77 @@
-function sayMyName(){
-    console.log("H");
-    console.log("I");
-    console.log("T");
-    console.log("E");
-    console.log("S");
-    console.log("H");
+function sayMyName() {
+  console.log("H");
+  console.log("I");
+  console.log("T");
+  console.log("E");
+  console.log("S");
+  console.log("H");
 }
 
 // sayMyName()
-// for execution we need to call the function 
+// for execution we need to call the function
 
 // function addTwoNumbers(number1, number2){
 
 //     console.log(number1 + number2);
 // }
 
-function addTwoNumbers(number1, number2){
-
-    // let result = number1 + number2
-    // return result
-    return number1 + number2
+function addTwoNumbers(number1, number2) {
+  // let result = number1 + number2
+  // return result
+  return number1 + number2;
 }
 
-const result = addTwoNumbers(3, 5)
+const result = addTwoNumbers(3, 5);
 
 // console.log("Result: ", result);
 
-function loginUserMessage(username = "sam"){
-  // sam is the default value 
-    if(!username){
-      // or use as username === undefined in the if statement 
-        console.log("PLease enter a username");
-        return
-    }
-    return `${username} just logged in`
+function loginUserMessage(username = "sam") {
+  // sam is the default value
+  if (!username) {
+    // or use as username === undefined in the if statement
+    console.log("PLease enter a username");
+    return;
+  }
+  return `${username} just logged in`;
 }
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage())
-// in this case undefined gets returned 
+// in this case undefined gets returned
+
+// Shopping cart where you dont know the no, of arguments that will come
+
+function calculateCartPrice(val1, val2, ...num1) {
+  // ...num1 is the rest operator
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+// num1 -- returns an array
+
+const user = {
+  username: "hitesh",
+  prices: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+
+// handleObject(user)
+handleObject({
+  username: "sam",
+  price: 399,
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
