af9b1473a2ace1600a144009bbd4c4b4686a0cc5
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-08-19 23:34:06 +0530

    filer in js , map , map and filter combination , reduce

---
+++ O16_Loops/O7_Map_CombinationOfMapAndFilter.js
@@ -0,0 +1,14 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+// in case of map it returns the value by default 
+
+// Chaining 
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
+// filter only true or false 
+// map - returns values 
