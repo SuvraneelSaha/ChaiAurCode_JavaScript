c717515ba2b1e5585fd9e29c0b32e42b141c49e6
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-08-20 12:28:52 +0530

    Project 1 - colour theme changer done

---
+++ O18_DOM_Projects/O1_Project_ColorChanger/chaiaurcode.js
@@ -0,0 +1,28 @@
+// selecting the buttons
+const buttons = document.querySelectorAll(".button");
+// console.log(buttons);
+const body = document.querySelector("body");
+
+// buttons is a nodelist
+
+buttons.forEach(function (button) {
+  console.log(button);
+
+  button.addEventListener("click", function (e) {
+    // e is the event
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+     if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+     if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+     if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
