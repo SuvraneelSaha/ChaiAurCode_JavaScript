a558d39022570e7fdd65482a16462d41fa280308
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-08-23 14:17:25 +0530

    understanding setInterval , clearInterval , setTimeout,clearTimeout

---
+++ O20_Async_Javascript/O1_Async.js
@@ -0,0 +1,21 @@
+// mainly - js is synchronous 
+// and single threaded 
+
+// this above is default 
+
+// Execution Context - memory , call stack , heap 
+// execute line by line 
+// log 1 - first 
+// log 2 - secind 
+// each operation wauts for the last one to complete before executing 
+
+// Blocking code vs Non blocking code 
+// block the flow of the program 
+// ex - read file - sync 
+
+// non blocking code 
+// does not block execution 
+// read file - async 
+
+// depends on the use case - example user registration 
+
